20a33a1b8754e67b5558f38bd02fe1ba12452ceb
YamanAyoun <yamanayoun0@gmail.com>
2023-07-06 00:59:19 +0300

    add linked list insertion branch

--- linked-list-insertions/Program.cs
+++ linked-list-insertions/Program.cs
@@ -6,8 +6,18 @@ namespace linked_list_insertions
     {
         static void Main(string[] args)
         {
-            
-            
+
+            insertions_linked_list list = new();
+            list.Append("1");
+            list.Append("4");
+            list.Append("2");
+            list.Append("7");
+            Console.WriteLine("I want to Insert '5' After '4'");
+            list.InsertAfter("4", "5");
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("I want to Insert '0' before '1'");
+            list.InsertBefore("1", "0");
+            Console.WriteLine(list.ToString());
         }
     }
 }

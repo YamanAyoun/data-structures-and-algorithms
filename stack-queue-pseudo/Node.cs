edc5903fef3ceae142b5e37ebacc282d9a750e4d
YamanAyoun <yamanayoun0@gmail.com>
2023-07-16 14:56:05 +0300

    add stack-queue-pseudo

---
+++ stack-queue-pseudo/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_queue_pseudo
+{
+    public class Node
+    {
+        public string Data { get; set; }
+        public Node Next;
+
+        public Node(string Data)
+        {
+            this.Data = Data;
+            Next = null;
+        }
+    }
+}

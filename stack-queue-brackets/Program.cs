6346d84010162f8d6b3fe03fd8103006830f8a53
YamanAyoun <yamanayoun0@gmail.com>
2023-07-23 14:07:47 +0300

    add stack queue brackets

---
+++ stack-queue-brackets/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace stack_queue_brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidateBrackets("{}{Code}[Fellows](())"));
+            Console.WriteLine(ValidateBrackets("(]("));
+        }
+
+
+        
+            public static bool ValidateBrackets(string input)
+            {
+                if (input == "")
+                {
+                    return false;
+                }
+
+                string right = ")}]";
+                string left = "({[";
+
+                Stack<char> charStack = new Stack<char>();
+
+                foreach (var item in input)
+                {
+                    if (left.Contains(item))
+                    {
+                        charStack.Push(item);
+                    }
+                    else if (right.Contains(item))
+                    {
+                        if (charStack.Count < 1)
+                        {
+                            return false;
+                        }
+                        
+
+                        if (right.IndexOf(item) != left.IndexOf(charStack.Pop()))
+                        {
+                            return false;
+                        }
+                    }
+                }
+                
+                return charStack.Count == 0;
+            
+        }
+    }
+}

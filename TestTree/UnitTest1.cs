022c1ad49e4d869bf91a105cf5b3a8c16cbc72f3
YamanAyoun <yamanayoun0@gmail.com>
2023-07-29 22:46:35 +0300

    add tree

---
+++ TestTree/UnitTest1.cs
@@ -0,0 +1,91 @@
+using trees;
+
+namespace TestTree
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanInstantiateEmptyTree()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Assert.Null(tree.Root);
+        }
+
+        [Fact]
+        public void CanInstantiateTreeWithSingleRootNode()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(1);
+            Assert.Equal(1, tree.Root.Value);
+        }
+
+        [Fact]
+        public void CanAddLeftAndRightChildToNodeInBinarySearchTree()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+
+            Assert.Equal(5, tree.Root.Value);
+            
+            //child
+            Assert.Equal(3, tree.Root.Left.Value);
+            Assert.Equal(7, tree.Root.Right.Value);
+        }
+
+        [Fact]
+        public void CanReturnPreOrderTraversal()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(3);
+            tree.Add(1);
+            tree.Add(10);
+            
+            
+
+            List<int> PreOrder = new List<int> { 3, 1, 10};
+            
+            Assert.Equal(PreOrder, tree.PreOrderTravarsel());
+        }
+
+        [Fact]
+        public void CanReturnInOrderTraversal()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(3);
+            tree.Add(1);
+            tree.Add(10);
+            
+
+            List<int> InOrder = new List<int> { 1, 3, 10 };
+  
+            Assert.Equal(InOrder, tree.InorderTraversal());
+        }
+
+        [Fact]
+        public void CanReturnPostOrderTraversal()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(3);
+            tree.Add(1);
+            tree.Add(10);
+            
+
+            List<int> PostOrder = new List<int> { 1, 10, 3 };
+            
+            Assert.Equal(PostOrder, tree.PostorderTraversal());
+        }
+
+        [Fact]
+        public void TestContains()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(5);
+            tree.Add(10);
+
+            Assert.True(tree.Contains(10));
+            Assert.False(tree.Contains(12));
+        }
+    }
+}
---
+++ TestTrees/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestTrees
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

f1bf4a595ac0a5bcf949891e9758cbe963a1b68d
YamanAyoun <yamanayoun0@gmail.com>
2023-09-08 15:01:50 +0300

    add more test

--- TestSorting/UnitTest1.cs
+++ TestSorting/UnitTest1.cs
@@ -39,5 +39,38 @@ namespace TestSorting
             Assert.Equal(merged, arr);
         }
 
+        [Fact]
+        public void TestMergeSortIfEmpty()
+        {
+            int[] arr = { 8, 4, 23, 42, 16, 15 };
+            int[] merged = { 4, 8, 15, 16, 23, 42 };
+
+            arr = mergeSort.MergeSort(arr);
+
+            Assert.Equal(merged, arr);
+        }
+
+
+        [Fact]
+        public void TestMergeSortWithSingleElement()
+        {
+            int[] arr = { 1 };
+            int[] merged = { 1 };
+
+            arr = mergeSort.MergeSort(arr);
+
+            Assert.Equal(merged, arr);
+        }
+
+        [Fact]
+        public void TestMergeSortIfDuplicateElements()
+        {
+            int[] arr = { 3, 2, 1, 3, 2, 1 };
+            int[] merged = { 1, 1, 2, 2, 3, 3 };
+
+            arr = mergeSort.MergeSort(arr);
+
+            Assert.Equal(merged, arr);
+        }
     }
 }
